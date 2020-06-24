665829dfb3303e6043fa9cbf93df76cc73e48337
MartinNikolov <martin.v.nikolov93@gmail.com>
2020-06-24 19:11:33 +0300

    Removed "&" from folder name since it won't let bcrypt installation from npm

--- Cubicle-Workshop-3-Session-And-Authentication/routes/auth.js
+++ Cubicle-Workshop-3-Session-And-Authentication/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 router.get('/login', (req, res) => {
     res.render('loginPage')
@@ -7,6 +8,14 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
     res.render('registerPage')
+})
+
+router.post('/signup', async (req, res) => {
+    const {username, password} = req.body
+    const user = new User({username, password})
+    await user.save()
+
+    res.redirect('/')
 })
 
 module.exports = router
